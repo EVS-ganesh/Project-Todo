d20436a79cd57833da7b823aa2d6deb8896557d5
Venkat <epuruvenkatasaiganesh@gmail.com>
2023-04-05 20:34:56 +0530

    changed to HTML tags , given sufficient spaces and changed the namings in css also

--- todo.js
+++ todo.js
@@ -1,70 +1,57 @@
-let Add=document.querySelector('#add');
-let Alltasks=document.querySelector('.alltasks');
-let Taskbar=document.querySelector('.taskbar');
-let Tasknamer=document.querySelector('#tasknamer');
-let Flist=document.querySelector('#flisted');
-let Searchbox=document.querySelector('#searchbox');
-Searchbox.addEventListener(aaa + "ss" + sss,function(){
-    if(Searchbox.value[0]==tasks.innerText[0]){
-    }
-})
-var aa = "ddd";
-Tasknamer.addEventListener("keypress",(event) => {
+const fList=document.querySelector('#flisted');
+const searchBox=document.querySelector('#searchbox');
+
+const taskNamer=document.querySelector('#tasknamer');
+taskNamer.addEventListener( "keypress" , (event) => {
     console.log(this);
 
-    
-    if(event.key==="Enter"){
-        if(Tasknamer.value=="" || Tasknamer.value[0]==" "){
+    if(event.key === "Enter" ){
+        if(taskNamer.value === "" || taskNamer.value[0] === " " ){
             console.log(`task ${event.value} not entered`);
         }
 
         else{
         
-        let tasks=document.createElement("div");
-        let para=document.createElement("p");
-        let favorite=document.createElement("div");
-        let notfavorite=document.createElement("div");
-        let editor=document.createElement("div");
-        let deletor=document.createElement("div");
+        let task = allTasks.innerHTML += `<div id="taskstyle">
+        <p id = "paragraph" > ${ taskNamer.value } </p>
+        <div id = "favo" ></div>
+        <div id = "nofavo" ></div>
+        <div id = "editElement" ></div>
+        <div id = "deleteElement" ></div>
+        <div>`
 
-        para.innerText=`${Tasknamer.value}`;
-        Alltasks.appendChild(tasks);
-        tasks.setAttribute("id","taskstyle");
-        favorite.setAttribute("id","favo");
-        favorite.setAttribute("id","favo");
-        notfavorite.setAttribute("id","nofavo");
-        editor.setAttribute("id","edit");
-        deletor.setAttribute("id","dele");
+        const allTasks=document.querySelector('.alltasks');
+        allTasks.appendChild(tasks);
 
-        tasks.appendChild(para);
-        tasks.appendChild(favorite);
-        tasks.appendChild(notfavorite);
-        tasks.appendChild(editor);
-        tasks.appendChild(deletor);
-
-        favorite.addEventListener("click",function(){
-          favorite.style.display="none";
-          notfavorite.style.display="block";
-          Flist.appendChild(tasks);
+        const favorite = document.getElementById("favo");
+        favorite.addEventListener( "click" , () => {
+          favorite.style.display = "none" ;
+          unfavorite.style.display = "block" ;
+          fList.appendChild(task);
         });
-
-        notfavorite.addEventListener("click",function(){
-            notfavorite.style.display="none";
-            favorite.style.display="block";
-            Alltasks.appendChild(tasks);
+ 
+        const unfavorite = document.getElementById("unfavo");
+        unfavorite.addEventListener( "click" , () => {
+            unfavorite.style.display = "none" ;
+            favorite.style.display = "block" ;
+            allTasks.appendChild(task);
           });
 
-        editor.addEventListener("click",function(){
-           para.contentEditable="true";
-           para.style.outline="none";
-           para.addEventListener("keypress",function(event){
-           if(event.key==="Enter"){
-                para.contentEditable="false";
+        const editElement = document.getElementById("editElement");
+        editElement.addEventListener( "click" , () => {
+           para.contentEditable = "true" ;
+           para.style.outline = "none" ;
+           para.addEventListener( "keypress" , (event) => {
+           if(event.key === "Enter" ){
+                para.contentEditable = "false" ;
             }
            })
+
         });
-       deletor.addEventListener("click",function(){
-        tasks.remove();
+
+       const deleteElement = document.getElementById("deleteElement");
+       deleteElement.addEventListener( "click" , function(){
+        task.remove();
        });
      }
     }
