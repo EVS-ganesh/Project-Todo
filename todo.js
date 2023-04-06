80c85639e0697b5350dd4f997a0341a2c1b73d6b
Venkat <epuruvenkatasaiganesh@gmail.com>
2023-04-06 10:45:26 +0530

    added forEach to work all elements and changed some of the querySelectors to querySelectorAll

--- todo.js
+++ todo.js
@@ -1,8 +1,7 @@
-const fList=document.querySelector('#flisted');
-const searchBox=document.querySelector('#searchbox');
+const searchBox = document.querySelector("#searchbox");
 
-const taskNamer=document.querySelector('#tasknamer');
-taskNamer.addEventListener( "keypress" , (event) => {
+const taskNamer = document.querySelector("#tasknamer");
+taskNamer.addEventListener("keypress" , (event) => {
 
     if(event.key === "Enter" ){
         if(taskNamer.value === "" || taskNamer.value[0] === " " ){
@@ -10,9 +9,10 @@ taskNamer.addEventListener( "keypress" , (event) => {
         }
 
         else{
-        
-        const allTasks=document.querySelector('.alltasks');
-        let task = allTasks.innerHTML += `<div id="taskstyle">
+        const allTasks=document.querySelector(".alltasks");
+
+        let task = document.createElement("div");
+        task.innerHTML = `<div id="taskstyle">
         <p id = "paragraph" > ${ taskNamer.value } </p>
         <div id = "favorite" ></div>
         <div id = "unfavorite" ></div>
@@ -20,37 +20,46 @@ taskNamer.addEventListener( "keypress" , (event) => {
         <div id = "deleteElement" ></div>
         <div>`;
         allTasks.appendChild(task);
-
-        const favorite = document.querySelector("#favorite");
-        const unfavorite = document.querySelector("#unfavorite");
-        favorite.addEventListener( "click" , () => {
-          favorite.style.display = "none" ;
-          unfavorite.style.display = "block" ;
-          fList.appendChild(task);
-        });
-
-        unfavorite.addEventListener( "click" , () => {
-            unfavorite.style.display = "none" ;
-            favorite.style.display = "block" ;
-            allTasks.appendChild(task);
+        
+        const fList = document.querySelector("#flisted");
+        const favorites = document.querySelectorAll("#favorite");
+        favorites.forEach(favorite => {
+          favorite.addEventListener("click" , () => {
+            favorite.style.display = "none" ;
+            favorite.nextElementSibling.style.display = "block" ;
+            fList.appendChild(favorite.parentNode.parentNode);
           });
-
-        const editElement = document.getElementById("editElement");
-        editElement.addEventListener( "click" , () => {
-           para.contentEditable = "true" ;
-           para.style.outline = "none" ;
-           para.addEventListener( "keypress" , (event) => {
-           if(event.key === "Enter" ){
-                para.contentEditable = "false" ;
-            }
-           })
-
         });
 
-       const deleteElement = document.getElementById("deleteElement");
-       deleteElement.addEventListener( "click" , function(){
-        task.remove();
-       });
-     }
+        const unfavorites = document.querySelectorAll("#unfavorite");
+        unfavorites.forEach(unfavorite =>{
+          unfavorite.addEventListener("click" , () => {
+            unfavorite.style.display = "none" ;
+            unfavorite.previousElementSibling.style.display = "block" ;
+            allTasks.appendChild(unfavorite.parentNode.parentNode);
+          });
+        })
+
+          const editElements = document.querySelectorAll("#editElement");
+          editElements.forEach(editElement =>{
+          const paragraph = document.querySelector("#paragraph");
+          editElement.addEventListener("click" , () => {
+             paragraph.contentEditable = "true" ;
+             paragraph.style.outline = "none" ;
+             paragraph.addEventListener("keypress" , (event) => {
+             if(event.key === "Enter" ){
+                  paragraph.contentEditable = "false" ;
+               }
+            })
+        });
+     })
+        
+       const deleteElements = document.querySelectorAll("#deleteElement");
+       deleteElements.forEach(deleteElement =>{
+         deleteElement.addEventListener("click" , function(){
+           deleteElement.parentNode.parentNode.remove();
+         });
+      });
     }
+  }
 });
